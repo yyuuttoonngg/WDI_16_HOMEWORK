a592a8f4f2d622ebde6b1d34e4ee2692d547e61c
Tong Yu <rachieyu@gmail.com>
2018-08-02 18:41:32 +1000

    homework 802 infinite giphy

---
+++ Tong/wk07/4-thu/app.js
@@ -0,0 +1,41 @@
+// srbOZIQ6Ewvyy6zxWsxCtkeuiOM880A0
+
+var offset = 0
+var searchWord;
+
+function getSearchWord(){
+    searchWord = $('input').val();
+    searchGiphy();
+}
+
+function searchGiphy(){
+    console.log('search')
+    $.ajax({
+        method: 'GET',
+        url: 'https://api.giphy.com/v1/gifs/search?q=' + searchWord + '&api_key=srbOZIQ6Ewvyy6zxWsxCtkeuiOM880A0&limit=10&offset='+offset,
+        dataType: 'json',
+    }).done(onSuccess);
+}
+
+function onSuccess(responseData) {
+    responseData["data"].forEach( element => {
+        var url = element["images"]["fixed_width"]["url"];
+        var img = document.createElement('img');
+        img.src=url;
+        $('.giphies').append(img);
+    })
+    offset+=10;
+}
+
+$(window).scroll(function() {
+    if($(window).scrollTop() == $(document).height() - $(window).height()){
+        if (offset<30){
+        searchGiphy()
+        } else {
+            $('.end-btn').removeClass('hidden');
+        }
+    }
+})
+
+$('.input-btn').on('click',getSearchWord);
+$('.end-btn').on('click',searchGiphy);
