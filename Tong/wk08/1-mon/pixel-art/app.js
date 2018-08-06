8d9a8cd8c215c35cd7863e2772c26228e786a5ac
Tong Yu <rachieyu@gmail.com>
2018-08-06 18:20:57 +1000

    pixel art commit 5

--- Tong/wk08/1-mon/pixel-art/app.js
+++ Tong/wk08/1-mon/pixel-art/app.js
@@ -1,26 +1,32 @@
+var userColor;
+
+(function getColor(){
+    $('.color-btn').on('click',function(e){
+        e.preventDefault();
+        userColor = $('.input-box').val();
+        console.log(userColor);
+    })
+    $('form').on('keydown', '.input-box', function (e) {
+        var key = e.which;
+        switch (key) {
+        case 13: 
+        e.preventDefault();
+            userColor = $('.input-box').val();
+            console.log(userColor);
+        }
+    })
+})();
+
+
 for(var i=0; i<1000; i ++){
     $('<div/>',{ class : 'square'}).appendTo('.canvas');
+    $('.square').on('click',changeColor);
 }
 
-$('.color-btn').on('click',function(e){
-    e.preventDefault();
-    var userColor = $('.input-box').val();
-    console.log(userColor);
-})
+function changeColor(){
+    $(this).css("background-color",userColor);
+}
 
-$('form').on('keydown', '.input-box', function (e) {
-    var key = e.which;
-    switch (key) {
-    case 13: // enter
-    e.preventDefault();
-        var userColor = $('.input-box').val();
-        console.log(userColor);
-    }
-});
-$('.square').each(function(){
-    $(this).on("click",function(){
-        $(this).toggleClass('green');
-    })
-})
+
 
 
