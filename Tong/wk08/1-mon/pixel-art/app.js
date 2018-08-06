00dea8ba2ca603ade1347212ca8b2636b749d688
Tong Yu <rachieyu@gmail.com>
2018-08-06 16:31:11 +1000

    pixel art commit 2

--- Tong/wk08/1-mon/pixel-art/app.js
+++ Tong/wk08/1-mon/pixel-art/app.js
@@ -1,4 +1,15 @@
 $('.color-btn').on('click',function(e){
     e.preventDefault();
-    var usercolor = $('.input-box').val();
+    var userColor = $('.input-box').val();
+    console.log(userColor);
 })
+
+$('form').on('keydown', '.input-box', function (e) {
+    var key = e.which;
+    switch (key) {
+    case 13: // enter
+    e.preventDefault();
+        var userColor = $('.input-box').val();
+        console.log(userColor);
+    }
+});
